3491083e2eedd820b2d588c39ad4ef1014fee68a
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-07-23 13:06:07 -0400

    Add normalization code to utils

--- utils.py
+++ utils.py
@@ -6,11 +6,33 @@ import ilpy
 
 from motile.constraints import Constraint
 from motile.variables import EdgeSelected, NodeAppear
+import numpy as np
 
 
 if TYPE_CHECKING:
     from motile.solver import Solver
 
+
+def normalize(x, pmin=3, pmax=99.8, axis=None, clip=False, eps=1e-20, dtype=np.float32):
+    """Percentile-based image normalization."""
+
+    mi = np.percentile(x,pmin,axis=axis,keepdims=True)
+    ma = np.percentile(x,pmax,axis=axis,keepdims=True)
+    return normalize_mi_ma(x, mi, ma, clip=clip, eps=eps, dtype=dtype)
+
+def normalize_mi_ma(x, mi, ma, clip=False, eps=1e-20, dtype=np.float32):
+    if dtype is not None:
+        x   = x.astype(dtype,copy=False)
+        mi  = dtype(mi) if np.isscalar(mi) else mi.astype(dtype,copy=False)
+        ma  = dtype(ma) if np.isscalar(ma) else ma.astype(dtype,copy=False)
+        eps = dtype(eps)
+
+    x = (x - mi) / ( ma - mi + eps )
+
+    if clip:
+        x = np.clip(x,0,1)
+
+    return x
 
 class InOutSymmetry(Constraint):
     r"""Ensures that all nodes, apart from the ones in the first and last
