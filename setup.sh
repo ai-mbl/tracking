45bb80e5a544399a73f0eda11d8b0584ed1dee9b
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-08-01 14:43:28 -0400

    Link to 2025 version of data in setup.sh

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ fi
 conda deactivate
 
 # Download data from s3
-wget https://dl-at-mbl-data.s3.us-east-2.amazonaws.com/2024/09_tracking/data.zip
+wget https://dl-at-mbl-data.s3.us-east-2.amazonaws.com/2025/09_tracking/data.zip
 unzip data.zip
 rm data.zip
 
